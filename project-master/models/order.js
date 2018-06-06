8fff34b3073a62cd478516599d519177886788e6
anmolmalik97 <anmolmalik97@gmail.com>
2018-06-06 17:07:13 +0530

    added checkout option

--- project-master/models/order.js
+++ project-master/models/order.js
@@ -2,11 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    cart: {type: Object, required: true},
-    address: {type: String, required: true},
-    name: {type: String, required: true},
-    paymentId: {type: String, required: true}
+    userid: 
+    {
+    	type: mongoose.Schema.Types.ObjectId,
+     	ref: 'User'
+ 	},
+    totalqty: 0,
+    totalprice: 0,
+    address: String,
+    paymentId: String
 });
 
 module.exports = mongoose.model('Order', schema);
